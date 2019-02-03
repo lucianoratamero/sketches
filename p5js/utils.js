404f7b9305fec8667cdc0f06945fbd4229675c91
Luciano Ratamero <luciano@ratamero.com>
2019-02-03 02:32:57 +0000

    adds new utils to the project.

--- p5js/utils.js
+++ p5js/utils.js
@@ -21,3 +21,22 @@ function range(start, stop, step) {
 
   return result;
 };
+
+function searchForArray(haystack, needle){
+  var stringifiedHaystack = JSON.stringify(haystack);
+  var stringifiedNeedle = JSON.stringify(needle);
+  return stringifiedHaystack.indexOf(stringifiedNeedle) >= 0;
+}
+
+function easeInOutN(t, power){
+  return t < 0.5 ? 0.5 * pow(2 * t, power) : 1 - 0.5 * pow(2 * (1 - t), power);
+}
+
+function timeCycle(totalframes, offset) {
+  var step = (frameCount + offset) % totalframes / totalframes;
+  return step;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
